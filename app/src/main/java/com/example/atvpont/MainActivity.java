180c1693df8aa105291b53936d030d7e18341478
Gabriel <gabrielolivleonidas@gmail.com>
2024-06-04 21:46:52 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/atvpont/Cadastro.java

---
+++ app/src/main/java/com/example/atvpont/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.atvpont;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewForgotPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // Implementar lógica de login
+            }
+        });
+
+        textViewForgotPassword.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // Implementar lógica de recuperação de senha
+            }
+        });
+    }
+}
